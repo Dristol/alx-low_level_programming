f153b181d089577fc96c4a23da9e9d259b66de3e
Idris Tolani Alade <134761672+Dristol@users.noreply.github.com>
2023-08-08 02:33:05 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	x = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
+	r = read(x, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
 	if (x == -1 || r == -1 || w == -1 || w != r)
