46c13e74344c0c1334892c1afad1b76de689750d
Idris Tolani Alade <134761672+Dristol@users.noreply.github.com>
2023-08-08 02:30:21 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,28 +10,29 @@
  **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int Fd;
-	ssize_t r, w;
-	char *buf;
 
-	if (!filename)
+	ssize_t x, r, w;
+	char *buffer;
+
+	if (filename == NULL)
 		return (0);
 
-	Fd = open(filename, O_RDONLY);
-
-	if (Fd == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	x = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (x == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
 		return (0);
+	}
 
-	r = read(Fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, r);
-
-	close(Fd);
-
-	free(buf);
+	free(buffer);
+	close(x);
 
 	return (w);
 }
